3bb33af3d53b746df474343b96e526596da871b3
Theodoros Evrenoglou <43581879+TEvrenoglou@users.noreply.github.com>
2024-11-15 16:43:57 +0100

    Update jags_bivariate_NMA.R

--- jags_bivariate_NMA.R
+++ jags_bivariate_NMA.R
@@ -26,51 +26,53 @@ model{
     
     prec2A[i,1:2,1:2] <- inverse(s[i,,])
     
-    y[(2*i-1):(2*i)] ~ dmnorm(mean[(2*i-1):(2*i)],prec2A[i,,])}
+    y[(2*i-1):(2*i)] ~ dmnorm(mean[(2*i-1):(2*i)],prec2A[i,,])
+    
+    }
   
   # three-arm studies
   
-  # for (i in 1:(Ns-N2h)){
-  #   S[i,1,1]<- var1[N2h+2*i-1]+psiR.sq
-  #   
-  #   S[i,2,2]<-var2[N2h+2*i-1]+psiD.sq
-  #   
-  #   S[i,3,3]<- var1[N2h+2*i]+psiR.sq
-  #   
-  #   S[i,4,4]<-var2[N2h+2*i]+psiD.sq
-  # 
-  #   S[i,1,2]<- control[i]*rho1*sqrt(S[i,1,1])*sqrt(S[i,2,2])
-  #   
-  #   S[i,2,1]<- S[i,1,2]
-  #   
-  #   S[i,1,3]<- control[i]*sqrt(S[i,1,1])*sqrt(S[i,3,3])/2
-  #   
-  #   S[i,3,1]<- S[i,1,3]
-  #   
-  #   S[i,1,4]<- control[i]*rho1*sqrt(S[i,1,1])*sqrt(S[i,4,4])/2
-  #   
-  #   S[i,4,1]<- S[i,1,4]
-  #   
-  #   S[i,2,3]<- control[i]*rho1*sqrt(S[i,2,2])*sqrt(S[i,3,3])/2
-  #   
-  #   S[i,3,2]<- S[i,2,3]
-  #   
-  #   S[i,2,4]<- control[i]*sqrt(S[i,2,2])*sqrt(S[i,4,4])/2
-  #   
-  #   S[i,4,2]<- S[i,2,4]
-  #   
-  #   S[i,4,3]<- control[i]*rho1*sqrt(S[i,4,4])*sqrt(S[i,3,3])
-  #   
-  #   S[i,3,4]<- S[i,4,3] 
-  # }
-  #   
-  # for (k in 1:(Ns-N2h)){
-  #   
-  #   prec3A[k,1:4,1:4] <- inverse(S[k,,])
-  #   
-  #   y[2*N2h+4*k-3:2*N2h+4*k]~dmnorm(mean[2*N2h+4*k-3:2*N2h+4*k],prec3A[k,,])
-  #   
-  #   }
+  for (i in 1:(Ns-N2h)){
+    S[i,1,1]<- var1[N2h+2*i-1]+psi1.sq
+
+    S[i,2,2] <-var2[N2h+2*i-1]+psi2.sq
+
+    S[i,3,3] <- var1[N2h+2*i]+psi1.sq
+
+    S[i,4,4] <- var2[N2h+2*i]+psi2.sq
+
+    S[i,1,2] <- control[i]*rho1*sqrt(S[i,1,1])*sqrt(S[i,2,2])
+
+    S[i,2,1] <- S[i,1,2]
+
+    S[i,1,3] <- control[i]*sqrt(S[i,1,1])*sqrt(S[i,3,3])/2
+
+    S[i,3,1] <- S[i,1,3]
+
+    S[i,1,4] <- control[i]*rho1*sqrt(S[i,1,1])*sqrt(S[i,4,4])/2
+
+    S[i,4,1] <- S[i,1,4]
+
+    S[i,2,3] <- control[i]*rho1*sqrt(S[i,2,2])*sqrt(S[i,3,3])/2
+
+    S[i,3,2] <- S[i,2,3]
+
+    S[i,2,4] <- control[i]*sqrt(S[i,2,2])*sqrt(S[i,4,4])/2
+
+    S[i,4,2] <- S[i,2,4]
+
+    S[i,4,3] <- control[i]*rho1*sqrt(S[i,4,4])*sqrt(S[i,3,3])
+
+    S[i,3,4] <- S[i,4,3]
+  }
+
+  for (k in 1:(Ns-N2h)){
+
+    prec3A[k,1:4,1:4] <- inverse(S[k,,])
+
+    y[(2*N2h+4*k-3):(2*N2h+4*k)] ~ dmnorm(mean[(2*N2h+4*k-3):(2*N2h+4*k)],prec3A[k,,])
+
+    }
   
   # Parameterization of the means
   
@@ -86,16 +88,17 @@ model{
     
     }
   
-  # for(i in 1:(Ns-N2h)) {
-  #   mean[2*N2h+4*i-3] <- d1[T2[N2h+i]] - d1[T1[N2h+i]]
-  #   
-  #   mean[2*N2h+4*i-2] <- d2[T2[N2h+i]] - d2[T1[N2h+i]]        
-  #   
-  #   mean[2*N2h+4*i-1] <- d1[T3[N2h+i]] - d1[T1[N2h+i]]
-  #   
-  #   mean[2*N2h+4*i] <- d2[T3[N2h+i]] - d2[T1[N2h+i]] 
+  for(i in 1:(Ns-N2h)) {
+    
+    mean[2*N2h+4*i-3] <- d1[T2[N2h+i]] - d1[T1[N2h+i]]
+
+    mean[2*N2h+4*i-2] <- d2[T2[N2h+i]] - d2[T1[N2h+i]]
+   
+    mean[2*N2h+4*i-1] <- d1[T3[N2h+i]] - d1[T1[N2h+i]]
+
+    mean[2*N2h+4*i] <- d2[T3[N2h+i]] - d2[T1[N2h+i]]
   #  
-  #  }	
+   }
   
   # Priors		
 
@@ -173,11 +176,12 @@ model{
   
   res.ref2[c] <- d2[c] - d2[ref]	
   
-  } 
+  }
   
   for (c in 1:(NT-1)) {
   
-    for (k in (c+1):NT) { 
+    for (k in (c+1):NT) {
+    
       res.2[c,k] <- d2[k] - d2[c] 
     }
       }	
